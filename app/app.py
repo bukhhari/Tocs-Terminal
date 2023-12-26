5b7d904683cd46a858d188ad99c6ed397c07344b
bukhhari <150888491+bukhhari@users.noreply.github.com>
2023-12-26 22:21:00 +0500

    Update app.py

--- app/app.py
+++ app/app.py
@@ -1,10 +1,10 @@
-from flask import Flask, render_template
+from flask import Flask
 
 app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-    return render_template('index.html')
+    return 'Hello, everyone this is our tocs terminal'
 
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0')
