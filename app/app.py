389c48414b3d9c84d443c85f82c68e7faed5f906
bukhhari <150888491+bukhhari@users.noreply.github.com>
2023-12-27 11:43:52 +0500

    Update app.py

--- app/app.py
+++ app/app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-    return 'Hello, everyone this welcome to the page rayed '
+    return 'Hello, everyone this page'
 
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0')
